9ed8e8d180094b5a15ec942c40e018b92614c490
lzhs <1050629507@qq.com>
2019-05-14 16:18:10 +0800

    添加 主页面 布局页面

--- app/src/main/java/com/lzhs/commondialog/MainActivity.kt
+++ app/src/main/java/com/lzhs/commondialog/MainActivity.kt
@@ -2,11 +2,21 @@ package com.lzhs.commondialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        mSmartRefresh.setEnableLoadMore(false)
+            .setEnableRefresh(false)
+            .setEnableOverScrollDrag(true)
     }
 }
